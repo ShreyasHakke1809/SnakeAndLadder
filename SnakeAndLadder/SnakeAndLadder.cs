b418e83762db430cc7e8e24720b542c18f595968
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-16 14:45:42 +0530

    Adding check for a option program to UC3

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -8,14 +8,34 @@ namespace SnakeAndLadder
 {
     internal class SnakeAndLadder
     {
+        const int NO_PLAY = 0;
+        const int LADDER = 1;
+        const int SNAKE = 2;
         public void StartPosition()
         {
             int startPosition = 0;
             int diceRoll;
+            int option;
             Console.WriteLine("Starting position of player is : " + startPosition);
             Random random = new Random();
             diceRoll = random.Next(1,7);
             Console.WriteLine("Player gets dice number :" + diceRoll);
+            option = random.Next(0,3);
+            switch(option)
+            {
+                case NO_PLAY:
+                    Console.WriteLine("No Play");
+                    break;
+                case LADDER:
+                    startPosition += diceRoll;
+                    Console.WriteLine("Ladder : "+ startPosition);
+                    break;
+                case SNAKE:
+                    startPosition -= diceRoll;
+                    Console.WriteLine("Snake : " + startPosition);
+                    break;
+
+            }
         }
     }
 }
