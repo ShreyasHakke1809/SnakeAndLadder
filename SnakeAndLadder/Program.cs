8fb9f730b1ec9fa4fb308b52d98dd1a10a2f0b5e
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-16 14:12:00 +0530

    Adding welcome massage to the main branch

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder program");
+            SnakeAndLadder snakeandladder = new SnakeAndLadder();
+            snakeandladder.Position();
+        }
+    }
+}
